e8a8fd78978533faa8e8e10f4938dc15e6797b95
ElOtakuQlo <130856683+FelipeGonzaUC@users.noreply.github.com>
2024-11-12 01:10:36 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,15 +3,43 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}`,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+      dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
   }
-};
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_test`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+      dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+  }
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_production`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+      dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+  }
+  }
+}
